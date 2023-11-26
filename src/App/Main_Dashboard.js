a30fd6454c1efe5dbbb42a8c1655bb0535a0f429
ManvendraSingh-1585 <78544885+ManvendraSingh-1585@users.noreply.github.com>
2023-11-26 19:59:08 +0530

    Add files via upload

---
+++ src/App/Main_Dashboard.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import logo from './../images/logo.png';
+import axios from 'axios';
+
+export default function Main_Dashboard() {
+  const storedUser = localStorage.getItem('user');
+  const user = JSON.parse(storedUser);
+  const user_id = user.user_id;
+  const [user_role, setUserRole] = useState('');
+  const username = user.user_name;
+  const navigate = useNavigate();
+
+  const handleOptionClick = (option) => {
+    // Step 1: Create the role
+    console.log(username);
+    axios
+      .post('http://localhost:8000/login/Main_Dashboard/createRole', { roleName: option })
+      .then((response) => {
+        const roleId = response.data.role_id; // Assuming the API returns the role ID
+
+        // Step 2: Associate the user with the role
+        return axios.post('http://localhost:8000/login/Main_Dashboard/assignRole', { user_id, roleId });
+      })
+      .then((response) => {
+        // Role assigned successfully
+        console.log('Role assigned:', response.data);
+
+        // Update the user_role state
+        setUserRole(option);
+
+        // You can redirect the user to a specific page after role assignment
+        navigate(`/login/Main_Dashboard/${option}`);
+      })
+      .catch((error) => {
+        // Handle errors related to role creation or assignment
+        console.error('Error:', error);
+      });
+  };
+
+  return (
+    <div id="container">
+      <nav id="navbar">
+        <a href="../"><img src={logo} alt="tournhub-logo" title="Tournhub" /></a>
+        <input type="checkbox" id="burger-toggle" />
+        <label id="burger" htmlFor="burger-toggle">
+          <div></div>
+        </label>
+        <ul>
+          <li>
+            <Link to="/" className="shortcut">Home</Link>
+          </li>
+          <li>
+            <Link to="/login/Main_Dashboard/Requests" className="shortcut">My-Requests</Link>
+          </li>
+          <li>
+            <Link to="/" className="shortcut">Log out</Link>
+          </li>
+        </ul>
+      </nav>
+      <section className="spread">
+        <h2>Dashboard for {username}</h2>
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <button
+            onClick={() => handleOptionClick('drivers')}
+            style={{
+              background: user_role === 'drivers' ? 'lightblue' : 'white',
+              marginRight: '5px',
+            }}
+          >
+            Create Ride
+          </button>
+          <button
+            onClick={() => handleOptionClick('Avail_Rides')}
+            style={{
+              background: user_role === 'Avail_Rides' ? 'lightblue' : 'white',
+              marginLeft: '5px',
+            }}
+          >
+            Book Ride
+          </button>
+        </div>
+      </section>
+    </div>
+  );
+}
---
+++ src/App/map.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css';
+import { useLocation, Link } from 'react-router-dom';
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoibm9vYmNvZGVyNjkiLCJhIjoiY2xnaHc4MXpnMDI5ejNobHZ4enE0cnZtMiJ9.KDG3W-eg9e8G41WojH5qpw'; // Replace with your Mapbox API token
+
+const MAP = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const directions = useRef(null);
+  const location = useLocation(); // Access the current location, including the query parameters
+
+  const params = new URLSearchParams(location.search);
+  const Driver = params.get('Driver');
+  const start = params.get('start');
+  const end = params.get('end');
+
+  useEffect(() => {
+    if (!map.current) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [83.011901, 25.321684], // Varanasi, India
+        zoom: 5, // Adjust the zoom level as needed
+      });
+
+      directions.current = new MapboxDirections({
+        accessToken: mapboxgl.accessToken,
+        unit: 'metric',
+        controls: {
+          instructions: false, // Hide step-by-step instructions
+          profileSwitcher: false, // Hide profile switcher (e.g., walking, cycling)
+        },
+      });
+
+      map.current.addControl(directions.current, 'top-left');
+    }
+
+    // Update the directions with new origin and destination
+    directions.current.setOrigin(start);
+    directions.current.setDestination(end);
+  }, [start, end]);
+
+  // Clean up map instance when component unmounts
+  useEffect(() => {
+    return () => {
+      if (map.current) {
+        map.current.remove();
+        map.current = null;
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h2>Route Of the Driver ({Driver})</h2>
+      </div>
+      <div ref={mapContainer} style={{ height: '500px' }} />
+      <h3 style={{ textAlign: 'center', marginTop: '40px' }}>
+        <Link to="/login/Main_Dashboard/Avail_Rides" className="shortcut">
+          Back
+        </Link>
+      </h3>
+    </div>
+  );
+};
+
+export default MAP;
